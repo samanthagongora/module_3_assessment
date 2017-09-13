7671e01421e62075090651fe5c486717fe7d22e9
Samantha Gongora <samantha.gongora@gmail.com>
2017-09-13 11:43:44 -0600

    adds service

--- app/models/store.rb
+++ app/models/store.rb
@@ -10,9 +10,7 @@ class Store
   end
 
   def self.find(zipcode)
-    @conn = Faraday.new(:url => 'https://api.bestbuy.com')
-    @response = @conn.get "/v1/stores(area(#{zipcode},25))?format=json&apiKey=#{ ENV['MY_KEY']}"
-    stores = JSON.parse(@response.body, :symbolize_names => true)[:stores]
+    stores = BestBuyService.find_stores(zipcode)
     stores.map do |raw_data|
       Store.new(raw_data)
     end
---
+++ app/services/BestBuyService.rb
@@ -0,0 +1,7 @@
+class BestBuyService
+
+@conn = Faraday.new(:url => 'https://api.bestbuy.com')
+@response = @conn.get "/v1/stores(area(#{zipcode},25))?format=json&apiKey=#{ ENV['MY_KEY']}"
+stores = JSON.parse(@response.body, :symbolize_names => true)[:stores]
+
+end
