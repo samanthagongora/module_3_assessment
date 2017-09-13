546e671ba6c59bdf7bb4253ebeaa1eda30f55b4c
Samantha Gongora <samantha.gongora@gmail.com>
2017-09-13 10:27:37 -0600

    abandons test for post, tested in postman, works

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,10 +3,15 @@ class Api::V1::ItemsController < API::V1::ApplicationController
     render json: Item.all
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
   def create
     item = Item.new(item_params)
     if item.save
-      redirect_back fallback_location: { action: show, id: item.id }
+      redirect_to api_v1_item_path(item)
     end
   end
 
@@ -15,4 +20,5 @@ class Api::V1::ItemsController < API::V1::ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :image_url)
   end
+
 end
