e5c48c72853f8fe8bd6eb5034b0eb9ef4b78c2b1
Samantha Gongora <samantha.gongora@gmail.com>
2017-09-13 09:35:22 -0600

    adds test for created and updated attributes on item

--- spec/requests/user_can_access_items_spec.rb
+++ spec/requests/user_can_access_items_spec.rb
@@ -2,14 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "As a user I can make an API call to Items" do
   scenario "and I can see all items ids, names, descriptions, and image_url" do
-    items = create_list(:item, 7)
+    all_items = create_list(:item, 7)
     get '/api/v1/items'
 
     expect(response).to be_successful
 
-    items = JSON.parse(response.body)
+    response_items = JSON.parse(response.body)
 
-    expect(items.count).to eq(7)
+    expect(response_items.count).to eq(7)
+    expect(response_items.first["id"]).to_not be_nil
+    expect(response_items.first["name"]).to_not be_nil
+    expect(response_items.first["description"]).to_not be_nil
+    expect(response_items.first["image_url"]).to_not be_nil
+    expect(response_items.first["created_at"]).to be_nil
+    expect(response_items.first["updated_at"]).to be_nil
   end
 end
 
