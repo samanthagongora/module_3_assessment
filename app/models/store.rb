7476831ff20be8d147ec197bb1f873ff6994fdbf
Samantha Gongora <samantha.gongora@gmail.com>
2017-09-13 11:34:57 -0600

    adds initialize and attr reader for store objects

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,10 +1,12 @@
 class Store
+  attr_reader :long_name, :store_type, :city, :distance, :phone_number
+
   def initialize(data)
-    @long_name = data[:longname]
+    @long_name = data[:longName]
     @city = data[:city]
     @distance = data[:distance]
     @phone_number = data[:phone]
-    @store_type = data[:storetype]
+    @store_type = data[:storeType]
   end
 
   def self.find(zipcode)
