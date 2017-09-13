b6aace1a3ccc3b589023938b94032b47762723b2
Samantha Gongora <samantha.gongora@gmail.com>
2017-09-13 11:27:15 -0600

    creates objects in store model

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,8 +1,18 @@
 class Store
+  def initialize(data)
+    @long_name = data[:longname]
+    @city = data[:city]
+    @distance = data[:distance]
+    @phone_number = data[:phone]
+    @store_type = data[:storetype]
+  end
+
   def self.find(zipcode)
     @conn = Faraday.new(:url => 'https://api.bestbuy.com')
     @response = @conn.get "/v1/stores(area(#{zipcode},25))?format=json&apiKey=#{ ENV['MY_KEY']}"
     stores = JSON.parse(@response.body, :symbolize_names => true)[:stores]
-    
+    stores.map do |raw_data|
+      Store.new(raw_data)
+    end
   end
 end
