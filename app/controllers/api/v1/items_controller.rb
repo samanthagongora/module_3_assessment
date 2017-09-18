5b33cc5942b2a2733cadf7211d23b4170fe56571
Samantha Gongora <samantha.gongora@gmail.com>
2017-09-17 19:39:10 -0600

    item serializer path

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,20 +1,20 @@
 class Api::V1::ItemsController < API::V1::ApplicationController
   def index
-    render json: Item.all
+    render json: Item.all, serializer: ItemSerializer
   end
 
   def show
     item = Item.find(params[:id])
-    render json: item
+    render json: item, serializer: ItemSerializer
   end
 
   def create
-    render status: 201, json: Item.create(item_params)
+    render status: 201, json: Item.create(item_params), serializer: ItemSerializer
   end
 
   def destroy
     item = Item.find(params[:id])
-    render status: 204, json: Item.destroy(item.id)
+    render status: 204, json: Item.destroy(item.id), serializer: ItemSerializer
   end
 
   private
